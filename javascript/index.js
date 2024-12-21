43a571fa7e58c514cc5c0923359f86cd02b0145b
Kaysee05 <159415699+Kaysee05@users.noreply.github.com>
2024-12-21 19:20:16 +0200

    Fixed spelling error in code

--- javascript/index.js
+++ javascript/index.js
@@ -20,7 +20,7 @@ let parisDateElement = parisElement.querySelector(".date");
 let parisTimeElement = parisElement.querySelector(".time");
 let parisTime = moment().tz("Europe/Paris");
 
-parisDateElemement.innerHTML = parisTime.format("MMMM Do YYYY");
+parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
 parisTimeElement.innerHTML = parisTime.format("h:mm:ss[<small>]A[</small>]");
 }
 }
